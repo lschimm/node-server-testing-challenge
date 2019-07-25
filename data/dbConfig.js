63249fa7786117b0083c885a2f845ebda0000ba5
lschimm <lschimmelfennig@gmail.com>
2019-07-25 17:37:17 -0500

    typo, oops

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require("knex");
 const config = require("../knexfile.js");
 require("dotenv").config();
 
-const environment = process.env.DB || "development";
+const environment = process.env.DB_ENV || "development";
 
 module.exports = knex(config[environment]);
