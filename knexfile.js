972a6077508b16df8ff81b469bb15b8f7e1fdd5b
lschimm <lschimmelfennig@gmail.com>
2019-07-25 17:36:04 -0500

    here goes

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
   testing: {
     client: "sqlite3",
     connection: {
-      filename: "./data/testing.bd3"
+      filename: "./data/testing.db3"
     },
     useNullAsDefault: true,
     migrations: {
